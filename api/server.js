c2af5b9c2010d3056e092a73946289c2adc0df38
David Tacoronte <d.tacoronte89@gmail.com>
2023-12-06 07:33:18 -0500

    UYsers by id on server.js working

--- api/server.js
+++ api/server.js
@@ -12,8 +12,28 @@ server.get('/api/users', (req, res) => {
             res.status(200).json(users);
         })
         .catch(err => {
+            res.status(404).json({
+                message: "does not exist",
+                err: err.message,
+                stack: err.stack,
+            })
+        })
+})
+
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+
+    User.findById(id)
+        .then(user => {
+            if(!user){
+                res.status(404).json({ message: "The user with the specified ID does not exist" })
+            }else{
+                res.status(200).json(user)
+            }
+        })
+        .catch(err => {
             res.status(500).json({
-                message: `Could not get users.`,
+                message: "Could not retrieve user.",
                 err: err.message,
                 stack: err.stack,
             })
