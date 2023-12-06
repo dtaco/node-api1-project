0853c45eff343ff891165cc6e9f3f3c31a7eeed3
David Tacoronte <d.tacoronte89@gmail.com>
2023-12-06 07:17:52 -0500

    find users path is working

--- api/server.js
+++ api/server.js
@@ -1,10 +1,23 @@
 
 const express = require('express')
-
 const server = express()
 
+const User = require('./users/model')
+
+
+
 server.get('/api/users', (req, res) => {
-    res.json('THE USERS!') //send status and info
+    User.find()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: `Could not get users.`,
+                err: err.message,
+                stack: err.stack,
+            })
+        })
 })
 
 server.use('*', (req, res) => {
